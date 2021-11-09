e15db08ec1db60d0ce8a11b71d5184d7e9839c25
Wenlei Xie <wxie@fb.com>
2021-11-09 01:41:40 -0800

    Minor change to copy/concat API (#54)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/torcharrow/pull/54
    
    1. Remove IColumn.copy since TorchArrow column is immutable, move it into IDataFrame.copy.
    2. Move IColumn.concat to torcharrow.concat
        * Pandas only has pandas.concat
        * Pandas's Series.append only accpet Series as parameter.
        * I think it makes sense to IColumn.append to support both Python list and Column as input. But not have both IColumn.append and IColumn.concat
    
    Reviewed By: dracifer
    
    Differential Revision: D32185907
    
    fbshipit-source-id: bb1a8b0d39fbf48278d701e10e8be3360cd3eb40

--- torcharrow/velox_rt/column.py
+++ torcharrow/velox_rt/column.py
@@ -1,11 +1,13 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+import typing as ty
+
 import torcharrow._torcharrow as velox
 from torcharrow import Scope
 from torcharrow.dispatcher import Device
 from torcharrow.dtypes import DType
 from torcharrow.icolumn import IColumn
 
-
+# TODO: Rename this class to IColumnVelox or IColumnCpu
 class ColumnFromVelox:
     _data: velox.BaseColumn
     _finialized: bool
@@ -24,7 +26,16 @@ class ColumnFromVelox:
 
     # Velox column returned from generic dispatch always assumes returned column is nullable
     # This help method allows to alter it based on context (e.g. methods in IStringMethods can have better inference)
+    #
+    # TODO: rename this as _with_null as alternating nullability is dangerous.
+    #   We should also infer the type nullability flag better with function metadata on Velox.
     def with_null(self, nullable: bool):
         return self.from_velox(
             self.device, self.dtype.with_null(nullable), self._data, True
         )
+
+    def _concat_with(self, columns: ty.List[IColumn]):
+        concat_list = self.to_pylist()
+        for column in columns:
+            concat_list += column.to_pylist()
+        return Scope._FromPyList(concat_list, self.dtype)
