2a04e088b83056c08f84408334ed2a73f1f592eb
Yuan Chao Chou <ychou@fb.com>
2021-12-08 10:50:44 -0800

    Support from_arrow_table
    Differential Revision: D32680115
    
    fbshipit-source-id: 3e72ecd7ec9c7c1920cfd6c694439d0d2efe3e51

--- torcharrow/interop.py
+++ torcharrow/interop.py
@@ -1,38 +1,49 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+from typing import Optional, List, Union
+
 import pandas as pd  # type: ignore
 import torcharrow.dtypes as dt
 from torcharrow.scope import Scope
 
 from .dispatcher import Dispatcher
+from .icolumn import IColumn
+from .idataframe import IDataFrame
+from .interop_arrow import _from_arrow_array, _from_arrow_table
 
 
-def from_arrow(data, dtype=None, device=""):
+def from_arrow(
+    data, dtype: Optional[dt.DType] = None, device: str = ""
+) -> Union[IColumn, IDataFrame]:
     """
     Convert arrow array/table to a TorchArrow Column/DataFrame.
     """
-    device = device or Scope.default.device
-
     import pyarrow as pa
-    from torcharrow._interop import _arrowtype_to_dtype
 
     assert isinstance(data, pa.Array) or isinstance(data, pa.Table)
 
-    dtype = dtype or _arrowtype_to_dtype(data.type, data.null_count > 0)
+    if dtype is not None:
+        raise NotImplementedError
+
     device = device or Scope.default.device
 
-    call = Dispatcher.lookup((dtype.typecode + "_fromarrow", device))
+    if isinstance(data, pa.Array):
+        return _from_arrow_array(data, device=device)
+    elif isinstance(data, pa.Table):
+        return _from_arrow_table(data, device=device)
+    else:
+        raise ValueError
 
-    return call(device, data, dtype)
 
-
-def from_pandas(data, dtype=None, device=""):
+def from_pandas(data, dtype: Optional[dt.DType] = None, device: str = ""):
     """
     Convert Pandas series/dataframe to a TorchArrow Column/DataFrame.
     """
     raise NotImplementedError
 
 
-def from_pylist(data, dtype=None, device=""):
+def from_pylist(
+    data: List, dtype: Optional[dt.DType] = None, device: str = ""
+) -> IColumn:
     """
     Convert Python list of scalars or containers to a TorchArrow Column/DataFrame.
     """
