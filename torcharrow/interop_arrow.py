ce56bc0375b0b58bdbeedc101a971b31909b3de6
Yuan Chao Chou <ychou@fb.com>
2021-12-08 19:58:37 -0800

    Support setting nullable via dtype in from_arrow
    Differential Revision: D32949563
    
    fbshipit-source-id: 2dede4c23705e84ba0a189cf31fc457ae0f08a8d

--- torcharrow/interop_arrow.py
+++ torcharrow/interop_arrow.py
@@ -1,6 +1,8 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 from typing import Optional
 
+import torcharrow.dtypes as dt
+
 from .dispatcher import Dispatcher
 from .icolumn import IColumn
 from .idataframe import DataFrame, IDataFrame
@@ -9,7 +11,7 @@ from .scope import Scope
 
 def _from_arrow_array(
     array,  # type: pa.Array
-    nullable: Optional[bool] = None,
+    dtype: Optional[dt.DType] = None,
     device: str = "",
 ) -> IColumn:
     device = device or Scope.default.device
@@ -19,17 +21,21 @@ def _from_arrow_array(
 
     assert isinstance(array, pa.Array)
 
-    if nullable is None:
-        # Using the most narrow type we can, we (i) don't restrict in any
-        # way where it can be used (since we can pass a narrower typed
-        # non-null column to a function expecting a nullable type, but not
-        # vice versa), (ii) when we bring in a stricter type system in Velox
-        # to allow functions to only be invokable on non-null types we
-        # increase the amount of places we can use the from_arrow result
-        nullable = array.null_count > 0
-    if not nullable and array.null_count > 0:
-        raise RuntimeError("Cannot store nulls in a non-nullable column")
-    dtype = _arrowtype_to_dtype(array.type, nullable)
+    # Using the most narrow type we can, we (i) don't restrict in any
+    # way where it can be used (since we can pass a narrower typed
+    # non-null column to a function expecting a nullable type, but not
+    # vice versa), (ii) when we bring in a stricter type system in Velox
+    # to allow functions to only be invokable on non-null types we
+    # increase the amount of places we can use the from_arrow result
+    dtype_from_arrowtype = _arrowtype_to_dtype(array.type, array.null_count > 0)
+    if dtype and (
+        dt.get_underlying_dtype(dtype) != dt.get_underlying_dtype(dtype_from_arrowtype)
+    ):
+        raise NotImplementedError("Type casting is not supported")
+    dtype = dtype or dtype_from_arrowtype
+
+    if not dtype.nullable and array.null_count > 0:
+        raise ValueError("Cannot store nulls in a non-nullable column")
 
     device = device or Scope.default.device
 
@@ -40,13 +46,17 @@ def _from_arrow_array(
 
 def _from_arrow_table(
     table,  # type: pa.Table
+    dtype: Optional[dt.DType] = None,
     device: str = "",
 ) -> IDataFrame:
     device = device or Scope.default.device
 
     import pyarrow as pa
+    from torcharrow._interop import _arrowtype_to_dtype
 
     assert isinstance(table, pa.Table)
+    if dtype:
+        assert dt.is_struct(dtype)
 
     # For now Arrow Table -> TA DataFrame is implemented by decomposing the table
     # into Arrow Arrays and then zero-copy converting the Arrays into TA Columns
@@ -63,9 +73,16 @@ def _from_arrow_table(
     df_data = {}
     for i in range(0, len(table.schema)):
         field = table.schema.field(i)
+
         assert len(table[i].chunks) == 1
         df_data[field.name] = _from_arrow_array(
-            table[i].chunk(0), field.nullable, device
+            table[i].chunk(0),
+            dtype=(
+                dtype.get(field.name)
+                if dtype is not None
+                else _arrowtype_to_dtype(field.type, field.nullable)
+            ),
+            device=device,
         )
 
     return DataFrame(df_data, device=device)
