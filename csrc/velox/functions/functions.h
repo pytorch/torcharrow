e9a2c503d0250d32f77d8f6e4be6256fdda918b7
Brandon Lin <branlin@fb.com>
2022-01-04 09:57:21 -0800

    Add log() method to numerical column (#128)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/torcharrow/pull/128
    
    Adds log() to the INumericalColumn class.
    
    Reviewed By: bhuang3
    
    Differential Revision: D33283607
    
    fbshipit-source-id: 6dc25384c231de776ef1047c00408a0e37a1fd4a

--- csrc/velox/functions/functions.h
+++ csrc/velox/functions/functions.h
@@ -1,6 +1,7 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
 #pragma once
 
+#include "numeric_functions.h"
 #include "string_functions.h"
 #include "velox/expression/VectorFunction.h"
 #include "velox/functions/lib/Re2Functions.h"
@@ -11,13 +12,28 @@ inline void registerTorchArrowFunctions() {
   velox::registerFunction<udf_torcharrow_isalpha, bool, velox::Varchar>();
   velox::registerFunction<udf_torcharrow_isalnum, bool, velox::Varchar>();
   velox::registerFunction<udf_torcharrow_isinteger, bool, velox::Varchar>();
-  velox::registerFunction<udf_torcharrow_isdecimal, bool, velox::Varchar>({"isdecimal"});
+  velox::registerFunction<udf_torcharrow_isdecimal, bool, velox::Varchar>(
+      {"isdecimal"});
   velox::registerFunction<udf_torcharrow_islower, bool, velox::Varchar>();
-  velox::registerFunction<udf_torcharrow_isupper, bool, velox::Varchar>({"isupper"});
+  velox::registerFunction<udf_torcharrow_isupper, bool, velox::Varchar>(
+      {"isupper"});
   velox::registerFunction<udf_torcharrow_isspace, bool, velox::Varchar>();
   velox::registerFunction<udf_torcharrow_istitle, bool, velox::Varchar>();
   velox::registerFunction<udf_torcharrow_isnumeric, bool, velox::Varchar>(
       {"isnumeric"});
+
+  velox::registerFunction<udf_torcharrow_log, float, float>({"torcharrow_log"});
+  velox::registerFunction<udf_torcharrow_log, double, double>(
+      {"torcharrow_log"});
+  velox::registerFunction<udf_torcharrow_log, float, bool>({"torcharrow_log"});
+  velox::registerFunction<udf_torcharrow_log, float, int8_t>(
+      {"torcharrow_log"});
+  velox::registerFunction<udf_torcharrow_log, float, int16_t>(
+      {"torcharrow_log"});
+  velox::registerFunction<udf_torcharrow_log, float, int32_t>(
+      {"torcharrow_log"});
+  velox::registerFunction<udf_torcharrow_log, float, int64_t>(
+      {"torcharrow_log"});
 
   velox::exec::registerStatefulVectorFunction(
       "match_re",
