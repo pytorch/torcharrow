9c398a361156973e29be868520ea9278db00ea52
Bowei Xu <boweixu@fb.com>
2022-02-01 14:07:12 -0800

    Fix floordiv and update to generate expr eagerly (#162)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/torcharrow/pull/162
    
    As title
    
    Reviewed By: wenleix, Tianshu-Bao
    
    Differential Revision: D33854926
    
    fbshipit-source-id: 2411564b189ca220a51800b1a9fa0c85836fba1d

--- csrc/velox/functions/numeric_functions.h
+++ csrc/velox/functions/numeric_functions.h
@@ -14,4 +14,27 @@ FOLLY_ALWAYS_INLINE bool call(TOutput& result, const TInput& input) {
 }
 VELOX_UDF_END();
 
+VELOX_UDF_BEGIN(torcharrow_floordiv_int)
+template <typename TOutput, typename TInput = TOutput>
+FOLLY_ALWAYS_INLINE bool call(TOutput& result, const TInput& a, const TInput& b) {
+  // Same as velox divide, when inputs are integers and b is 0, return error
+  // instead of inf because int(inf) will overflow.
+  if (b == 0) {
+    VELOX_ARITHMETIC_ERROR("division by zero");
+  }
+  // promote to float type to correctly compute floor divide for negative
+  // integers. e.g: -3 / 2 is -1, but floor(float(-3) / 2) is -2.
+  result = std::floor(float(a) / b);
+  return true;
+}
+VELOX_UDF_END();
+
+VELOX_UDF_BEGIN(torcharrow_floordiv)
+template <typename TOutput, typename TInput = TOutput>
+FOLLY_ALWAYS_INLINE bool call(TOutput& result, const TInput& a, const TInput& b) {
+  result = std::floor(a / b);
+  return true;
+}
+VELOX_UDF_END();
+
 } // namespace facebook::torcharrow::functions
