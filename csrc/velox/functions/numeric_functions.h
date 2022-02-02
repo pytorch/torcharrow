06a54b59b6a8bb14c49dc46a8001a0d3cde19e93
Bowei Xu <boweixu@fb.com>
2022-02-02 10:47:12 -0800

    Fix Modulus and update to generate expr eagerly (#165)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/torcharrow/pull/165
    
    Problem:
    TorchArrow mod operator works different from PyTorch/Pandas right now, because C++ and Python implement mod differently. Basically
    - in python, result of a % b is: a - floor(a / b)
    - in c++, result of a % b is: a - int(a / b)
    
    This diff address it with a new velox udf floormod
    
    Differential Revision: https://www.internalfb.com/diff/D33924566?entry_point=27
    
    fbshipit-source-id: c7cf2f87fac05421defabb822c461efd9cb4e259

--- csrc/velox/functions/numeric_functions.h
+++ csrc/velox/functions/numeric_functions.h
@@ -37,4 +37,29 @@ FOLLY_ALWAYS_INLINE bool call(TOutput& result, const TInput& a, const TInput& b)
 }
 VELOX_UDF_END();
 
+// Torcharrow_floormod generates same result as Python mod, which is different from C++.
+// Reference: "floored division" in https://en.wikipedia.org/wiki/Modulo_operation
+VELOX_UDF_BEGIN(torcharrow_floormod_int)
+template <typename TOutput, typename TInput = TOutput>
+FOLLY_ALWAYS_INLINE bool call(TOutput& result, const TInput& a, const TInput& b) {
+  // Same as velox modulus, when inputs are integers and b is 0, return error
+  // instead of inf because int(inf) will overflow.
+  if (b == 0) {
+    VELOX_ARITHMETIC_ERROR("Cannot divide by 0");
+  }
+  // promote to float type to correctly compute floor divide for negative
+  // integers. e.g: -3 / 2 is -1, but floor(float(-3) / 2) is -2.
+  result = a - std::floor(float(a) / b) * b;
+  return true;
+}
+VELOX_UDF_END();
+
+VELOX_UDF_BEGIN(torcharrow_floormod)
+template <typename TOutput, typename TInput = TOutput>
+FOLLY_ALWAYS_INLINE bool call(TOutput& result, const TInput& a, const TInput& b) {
+  result = a - std::floor(a / b) * b;
+  return true;
+}
+VELOX_UDF_END();
+
 } // namespace facebook::torcharrow::functions
