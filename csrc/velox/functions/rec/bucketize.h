23b98bf7fd0cc49a0d1e8f0f90929ad75d2405bd
Ying Liu <yingliufb@fb.com>
2022-03-17 11:09:20 -0700

    Velox bucketize implementation (#242)
    Summary:
    X-link: https://github.com/facebookresearch/torcharrow/pull/242
    
    We port over the Koski implementation of Bucketize to Velox
    
    Reviewed By: wenleix
    
    Differential Revision: D34629579
    
    fbshipit-source-id: 4c0699bf5a613e43a4943cc07f716410c8d14ebd

---
+++ csrc/velox/functions/rec/bucketize.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <velox/functions/Macros.h>
+#include "velox/core/QueryConfig.h"
+#include "velox/expression/ComplexViewTypes.h"
+#include "velox/functions/Udf.h"
+#include "velox/type/Type.h"
+
+namespace facebook::torcharrow::functions {
+
+template <typename T1>
+inline void validateBordersSpec(const T1& borders) {
+  VELOX_CHECK(borders.size() != 0, "Borders should not be empty.");
+  for (auto i = 1; i < borders.size(); ++i) {
+    VELOX_CHECK(
+        !(borders[i] < borders[i - 1]),
+        "Borders should have non-decreasing sequence.");
+    if (i > 1 && (borders[i] == borders[i - 1])) {
+      if (!(borders[i - 2] < borders[i])) {
+        std::string err_detail = "";
+        for (auto b : borders) {
+          err_detail += std::to_string(b) + ", ";
+        }
+        VELOX_CHECK(
+            (borders[i - 2] < borders[i]) || (borders[i - 2] == borders[i]),
+            "Borders should not have more than 2 repeated values, got: loc {}, array: {}",
+            i,
+            err_detail);
+      }
+    }
+  }
+}
+
+template <typename T1, typename T2>
+inline int32_t computeBucketId(const T1& borders, const T2& val) {
+  int32_t index = borders.size();
+  auto jump = borders.size();
+
+  while (jump > 0) {
+    while (index - jump >= 0 && val <= borders[index - jump]) {
+      index -= jump;
+    }
+    jump /= 2;
+  }
+  return index;
+}
+
+template <typename T>
+struct bucketize {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  template <typename TInput, typename TBordersInput>
+  FOLLY_ALWAYS_INLINE void callNullFree(
+      int32_t& result,
+      const TInput& val,
+      const TBordersInput& borders) {
+    validateBordersSpec(borders);
+    result = computeBucketId(borders, val);
+  }
+
+  template <typename TOutput, typename TInput, typename TBordersInput>
+  FOLLY_ALWAYS_INLINE void callNullFree(
+      TOutput& result,
+      const TInput& values,
+      const TBordersInput& borders) {
+    validateBordersSpec(borders);
+    for (const auto& val : values) {
+      result.push_back(computeBucketId(borders, val));
+    }
+  }
+};
+
+} // namespace facebook::torcharrow::functions
---
+++ csrc/velox/functions/rec/tests/BucketizeTest.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <cstdint>
+#include <optional>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <velox/common/base/VeloxException.h>
+#include <velox/vector/SimpleVector.h>
+#include <velox/vector/tests/VectorMaker.h>
+
+#include "pytorch/torcharrow/csrc/velox/functions/functions.h"
+#include "velox/functions/prestosql/tests/FunctionBaseTest.h"
+
+namespace facebook::velox {
+namespace {
+
+class BucketizeTest : public functions::test::FunctionBaseTest {
+ protected:
+  static void SetUpTestCase() {
+    torcharrow::functions::registerTorchArrowFunctions();
+  }
+
+  template <typename T, typename T1 = T, typename TExpected = T>
+  void assertArrayExpression(
+      const std::string& expression,
+      const std::vector<T>& arg0,
+      const std::vector<T1>& arg1,
+      const std::vector<TExpected>& expected) {
+    auto vector0 = makeFlatVector(arg0);
+    auto vector1 = makeArrayVector(arg1);
+
+    auto result = evaluate<SimpleVector<TExpected>>(
+        expression, makeRowVector({vector0, vector1}));
+
+    assertEqualVectors(
+        velox::test::VectorTestBase::makeFlatVector(expected), result);
+  }
+};
+
+TEST_F(BucketizeTest, IntInputFloatBorders) {
+  const std::vector<int32_t> a = {1, 2, 3, 4, 5, 6};
+  std::vector<std::vector<float>> allBorders(
+      a.size(), {0.0f, 2.0f, 3.0f, 5.0f, 500.0f, 1000.0f});
+  assertArrayExpression<int32_t, std::vector<float>, int32_t>(
+      "bucketize(c0, c1)", a, allBorders, {1, 1, 2, 3, 3, 4});
+}
+
+TEST_F(BucketizeTest, FloatInputFloatBorders) {
+  const std::vector<float> a = {1.5f, 2.5f, 3.5f, 4.5f, 5.5f, 6.5f};
+  std::vector<float> borders{0.0f, 2.0f, 3.0f, 5.0f, 500.0f, 1000.0f};
+  std::vector<std::vector<float>> allBorders(a.size(), borders);
+  assertArrayExpression<float, std::vector<float>, int32_t>(
+      "bucketize(c0, c1)", a, allBorders, {1, 2, 3, 3, 4, 4});
+}
+
+TEST_F(BucketizeTest, IntInputIntBorders) {
+  const std::vector<int32_t> a = {1, 2, 3, 4, 5, 6};
+  std::vector<std::vector<float>> allBorders(a.size(), {0, 2, 3, 5, 500, 1000});
+  assertArrayExpression<int32_t, std::vector<float>, int32_t>(
+      "bucketize(c0, c1)", a, allBorders, {1, 1, 2, 3, 3, 4});
+}
+
+TEST_F(BucketizeTest, FloatInputIntBorders) {
+  const std::vector<float> a = {1.5f, 2.5f, 3.5f, 4.5f, 5.5f, 6.5f};
+  std::vector<float> borders{0, 2, 3, 5, 500, 1000};
+  std::vector<std::vector<float>> allBorders(a.size(), borders);
+  assertArrayExpression<float, std::vector<float>, int32_t>(
+      "bucketize(c0, c1)", a, allBorders, {1, 2, 3, 3, 4, 4});
+}
+
+TEST_F(BucketizeTest, DecreasingBorder) {
+  const std::vector<float> a = {1.5f};
+  std::vector<std::vector<float>> allBorders(a.size(), {3.0f, 2.0f, 1.0f});
+  // TODO: maybe use ASSERT_THROW_KEEP_AS_E to check error msg.
+  EXPECT_THROW(
+      (assertArrayExpression<float, std::vector<float>, int32_t>(
+          "bucketize(c0, c1)", a, allBorders, {})),
+      velox::VeloxException);
+}
+
+TEST_F(BucketizeTest, RepeatedBorderValuesOk) {
+  const std::vector<float> a = {1.5f};
+  std::vector<std::vector<float>> allBorders(a.size(), {1, 2, 2, 3, 3});
+  assertArrayExpression<float, std::vector<float>, int32_t>(
+      "bucketize(c0, c1)", a, allBorders, {1});
+}
+
+TEST_F(BucketizeTest, FloatIdListInput) {
+  std::vector<std::vector<float>> a = {{1.5f, 2.5f, 150.0f}, {10.0f, 200.0f}};
+
+  std::vector<float> borders{1.0f, 10.0f, 100.0f};
+  std::vector<std::vector<float>> allBorders(a.size(), borders);
+
+  std::vector<std::vector<int32_t>> expected = {{1, 1, 3}, {1, 3}};
+
+  auto vector0 = makeArrayVector(a);
+  auto vector1 = makeArrayVector(allBorders);
+
+  auto result = evaluate<velox::ArrayVector>(
+      "bucketize(c0, c1)", makeRowVector({vector0, vector1}));
+
+  assertEqualVectors(
+      velox::test::VectorTestBase::makeArrayVector(expected), result);
+}
+
+TEST_F(BucketizeTest, IntIdListInput) {
+  std::vector<std::vector<int32_t>> a = {{1, 2, 150}, {10, 200}};
+
+  std::vector<float> borders{1.0f, 10.0f, 100.0f};
+  std::vector<std::vector<float>> allBorders(a.size(), borders);
+
+  std::vector<std::vector<int32_t>> expected = {{0, 1, 3}, {1, 3}};
+
+  auto vector0 = makeArrayVector(a);
+  auto vector1 = makeArrayVector(allBorders);
+
+  auto result = evaluate<velox::ArrayVector>(
+      "bucketize(c0, c1)", makeRowVector({vector0, vector1}));
+
+  assertEqualVectors(
+      velox::test::VectorTestBase::makeArrayVector(expected), result);
+}
+
+} // namespace
+} // namespace facebook::velox
