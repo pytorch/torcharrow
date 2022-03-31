b00f2875abf7787fdc42546b89236800ad56f167
Wenlei Xie <wenlei.xie@gmail.com>
2022-03-31 09:47:29 -0700

    Initial docs for TorchArrow (#269)
    Summary:
    Initial skeleton. More docs as future work.
    
    Pull Request resolved: https://github.com/facebookresearch/torcharrow/pull/269
    
    Reviewed By: NivekT
    
    Differential Revision: D35255896
    
    Pulled By: wenleix
    
    fbshipit-source-id: eafe44ff0b41c26cbb8333c782628b39cd2b88ed

---
+++ docs/source/conf.py
@@ -0,0 +1,81 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+import pytorch_sphinx_theme
+import torcharrow
+
+# sys.path.insert(0, os.path.abspath('.'))
+
+current_dir = os.path.dirname(__file__)
+target_dir = os.path.abspath(os.path.join(current_dir, "../.."))
+sys.path.insert(0, target_dir)
+print(target_dir)
+
+
+# -- Project information -----------------------------------------------------
+
+project = "TorchArrow"
+copyright = "2021 - Present, Torch Contributors"
+author = "Torch Contributors"
+
+# The short X.Y version
+version = "main (" + torcharrow.__version__ + " )"
+
+# The full version, including alpha/beta/rc tags
+release = "main"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_template/class.rst", "_template/function.rst"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+html_theme = "pytorch_sphinx_theme"
+html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
+
+html_theme_options = {
+    "collapse_navigation": False,
+    "display_version": True,
+    "logo_only": True,
+    "pytorch_project": "docs",
+    "navigation_with_keys": True,
+    "analytics_id": "UA-117752657-2",
+}
+
+
+def setup(app):
+    pass
