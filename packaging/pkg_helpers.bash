554891fd7b616556e28ef19f73007dad556d5a61
Xiong Zhang <bearzx@gmail.com>
2022-05-27 00:21:03 -0700

    shell env vars test

--- packaging/pkg_helpers.bash
+++ packaging/pkg_helpers.bash
@@ -103,8 +103,8 @@ setup_build_version() {
 # Set some useful variables for OS X, if applicable
 setup_macos() {
   if [[ "$(uname)" == Darwin ]]; then
-    export MACOSX_DEPLOYMENT_TARGET=10.15 CC=clang CXX=clang++
-    export bla=hi
+    export MACOSX_DEPLOYMENT_TARGET=10.16 CC=clang CXX=clang++
+    export bla=1.2
   fi
 }
 
