pytorch/torcharrow

commit bfb7b01e509179c82f668b958a7cdcb7d52064d4

Move VariantToVector to velox/vector (from velox/parse) (#499)
Summary:
Pull Request resolved: https://github.com/pytorch/torcharrow/pull/499

X-link: https://github.com/facebookincubator/velox/pull/2679

Reviewed By: Yuhta

Differential Revision: D39920178

Pulled By: mbasmanova

fbshipit-source-id: cd1a7ec721bddb58f0abd11ef27ed191617dc7aa

csrc/velox/VariantToVector.cpp
@@ -10,7 +10,7 @@
 // https://github.com/facebookincubator/velox/blob/18aff402c2b5a070ef7ec3f33cde017e90c8aa8a/velox/parse/VariantToVector.cpp
 // since parse is not part of VELOX_MINIMAL
 
-#include "velox/parse/VariantToVector.h"
+#include "velox/vector/VariantToVector.h"
 #include "velox/buffer/StringViewBufferHolder.h"
 #include "velox/type/Variant.h"
 #include "velox/vector/ComplexVector.h"

commit f5cfd20b508fe82973ee9ff8f9ca1fd0089265f5

Update header file in VariantToVector.cpp
This file is forked, and broken in bfb7b01e509179c82f668b958a7cdcb7d52064d4

csrc/velox/VariantToVector.cpp
@@ -10,7 +10,7 @@
 // https://github.com/facebookincubator/velox/blob/18aff402c2b5a070ef7ec3f33cde017e90c8aa8a/velox/parse/VariantToVector.cpp
 // since parse is not part of VELOX_MINIMAL
 
-#include "velox/vector/VariantToVector.h"
+#include "VariantToVector.h"
 #include "velox/buffer/StringViewBufferHolder.h"
 #include "velox/type/Variant.h"
 #include "velox/vector/ComplexVector.h"